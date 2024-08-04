ecc25ee1a24c13202649650bc3296ac8c64bd56d
Denys <denis.itsme.1@gmail.com>
2024-08-04 13:11:19 +0300

    add task-3

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,16 @@
 import { Grid, GridItem, TodoListItem } from 'components';
-export const TodoList = ({ todosData }) => {
+export const TodoList = ({ todosData, onDelete, onEdit }) => {
   return (
     <Grid>
       {todosData.map((todoItem, index) => (
         <GridItem key={todoItem.id}>
-          <TodoListItem index={index} text={todoItem.text} />
+          <TodoListItem
+            id={todoItem.id}
+            index={index}
+            text={todoItem.text}
+            onDelete={onDelete}
+            onEdit={onEdit}
+          />
         </GridItem>
       ))}
     </Grid>
