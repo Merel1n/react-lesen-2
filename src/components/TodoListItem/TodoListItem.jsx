84782f8c37f16f615eba48bd467fd9ea37e167eb
Olena Piatkivska <157681009+olenapiatkivska@users.noreply.github.com>
2024-09-01 13:28:08 +0300

    add task-11

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,8 +1,12 @@
 import { Text } from 'components';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 import style from './TodoListItem.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from 'reduxStore/todosOps';
+import { setCurrentTodo } from 'reduxStore/todosSlice';
 
-export const TodoListItem = ({ index, text, onDelete, id, onEdit }) => {
+export const TodoListItem = ({ index, text, id }) => {
+  const dispatch = useDispatch();
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
@@ -12,14 +16,14 @@ export const TodoListItem = ({ index, text, onDelete, id, onEdit }) => {
       <button
         className={style.deleteButton}
         type="button"
-        onClick={() => onDelete(id)}
+        onClick={() => dispatch(deleteTodo(id))}
       >
         <RiDeleteBinLine size={24} />
       </button>
       <button
         className={style.editButton}
         type="button"
-        onClick={() => onEdit({ id, text })}
+        onClick={() => dispatch(setCurrentTodo({ id, text }))}
       >
         <RiEdit2Line size={24} />
       </button>
